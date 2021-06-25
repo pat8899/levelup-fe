baf61f3b3de76907b3e4e743ae8ab8da0a6570bc
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 11:28:04 +1200

    Create NavBar & Button Components

---
+++ src/SharedComponents/Buttons/EnrolNowBtn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./EnrolNowBtn.css";
+
+function EnrolNowBtn() {
+  return (
+    <div className="EnrolNowBtnContainer">
+      <a href="/" className="EnrolNowBtn">
+        Enrol Now
+      </a>
+    </div>
+  );
+}
+
+export default EnrolNowBtn;
--- src/SharedComponents/Navbar/Navbar.js
+++ src/SharedComponents/Navbar/Navbar.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import EnrolNowBtn from "../Buttons/EnrolNowBtn";
+import StudentPortalBtn from "../Buttons/StudentPortalBtn";
 
 function Navbar() {
-    return (
-        <div>
-            <div className="navbar_container">
-            <h1>NAV BAR</h1>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div>
+      <div className="navbar_container">
+        <a href="/" className="Links Logo">
+          LOGO
+        </a>
+        <a href="/" className="Links NavBarOption">
+          Our Course
+        </a>
+        <a href="/" className="Links NavBarOption StudentProjects">
+          Student Projects
+        </a>
+        <a href="/" className="Links NavBarOption">
+          About Us
+        </a>
+        <a href="/" className="Links NavBarOption Contact">
+          Contact
+        </a>
+        {/*Height looks wrong might need to adjust*/}
+        <EnrolNowBtn />
+        <StudentPortalBtn />
+      </div>
+    </div>
+  );
+}
 
-export default Navbar
+export default Navbar;
