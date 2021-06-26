78e53ecfcc0fbd81e13194acb41aa3609806641a
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 18:51:26 +1200

    Course reviews component layout complete

--- src/Pages/Home/HomeComponents/CourseReviews.jsx
+++ src/Pages/Home/HomeComponents/CourseReviews.jsx
@@ -1,15 +1,88 @@
 import React from "react";
 import "./CourseReviews.css";
-
-{
-  /*Pull from database*/
-}
+import NavArrowLeft from "../../../Images/NavArrowLeft.png";
+import NavArrowRight from "../../../Images/NavArrowRight.png";
 
 function CourseReviews() {
   return (
-    <div>
-      <div>
-        <h1>Course Reviews</h1>
+    <div className="CourseReviewsContainer">
+      <h1 className="CourseReviewsHeading">Course Reviews</h1>
+      <div className="CourseReviews">
+        <img src={NavArrowLeft} alt="Arrow Left" className="NavArrowLeft" />
+
+        {/* Replace with props */}
+
+        <div className="ReviewOpen">
+          <div className="PlaceholderReviewImg"></div>
+          <div className="ReviewOpenContainer">
+            <h2 className="ReviewHeading">Best Course!</h2>
+            <p className="ReviewBody">
+              Lorem ipsum dolor sit amet, <br />
+              consectetur adipiscing elit, sed do
+              <br />
+              eiusmod tempor incididunt ut
+              <br />
+              labore et dolore magna aliquar.
+            </p>
+            <div className="ReviewContent">
+              <div className="PlaceholderParentPhoto"></div>
+              <div>
+                <h3 className="ParentName">Jane Doe</h3>
+                <p className="ParentIdentifier">Proud Mother</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Replace with props */}
+
+        <div className="ReviewClosed">
+          <h2 className="ReviewHeading">Loved it</h2>
+          <p className="ReviewBody">
+            Lorem ipsum dolor sit amet,
+            <br />
+            consectetur adipiscing elit,
+            <br />
+            sed do eiusmod tempor
+            <br />
+            incididunt ut labore et
+            <br />
+            dolore magna aliquar.
+          </p>
+          <div className="ReviewContentClosed">
+            <div className="PlaceholderParentPhoto"></div>
+            <div>
+              <h3 className="ParentName">Jane Doe</h3>
+              <p className="ParentIdentifier">Proud Mother</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Replace with props */}
+
+        <div className="ReviewClosed">
+          <h2 className="ReviewHeading">Loved it</h2>
+          <p className="ReviewBody">
+            Lorem ipsum dolor sit amet,
+            <br />
+            consectetur adipiscing elit,
+            <br />
+            sed do eiusmod tempor
+            <br />
+            incididunt ut labore et
+            <br />
+            dolore magna aliquar.
+          </p>
+          <div className="ReviewContentClosed">
+            <div className="PlaceholderParentPhoto"></div>
+            <div>
+              <h3 className="ParentName">Jane Doe</h3>
+              <p className="ParentIdentifier">Proud Mother</p>
+            </div>
+          </div>
+        </div>
+
+        <img src={NavArrowRight} alt="Arrow Right" className="NavArrowRight" />
       </div>
     </div>
   );
