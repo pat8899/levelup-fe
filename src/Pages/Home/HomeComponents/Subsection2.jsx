1c184817381444456db865e72f3499b4ad3120fd
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 17:04:34 +1200

    Home - subsection 2 component complete

--- src/Pages/Home/HomeComponents/Subsection2.jsx
+++ src/Pages/Home/HomeComponents/Subsection2.jsx
@@ -1,15 +1,55 @@
 import React from "react";
-import "./Subsection2";
-
-{
-  /*Course Benefits */
-}
+import "./Subsection2.css";
 
 function Subsection2() {
   return (
-    <div>
+    <div className="BenefitsContainer">
       <div>
-        <h1>Subsection2</h1>
+        <h1 className="BenefitHeading">
+          What are the benefits of this course?
+        </h1>
+      </div>
+      <div className="Benefits">
+        <div>
+          {/*Replace benefit with probs after further development */}
+          <div className="BenefitImgPlaceholder"></div>
+          <h3 className="BenefitSubheading">Lorum Ipsum</h3>
+          <p className="BenefitDescription">
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+            <br />
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+          </p>
+        </div>
+        <div className="Benefit2">
+          <div className="BenefitImgPlaceholder"></div>
+          <h3 className="BenefitSubheading">Lorum Ipsum</h3>
+          <p className="BenefitDescription">
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+            <br />
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+          </p>
+        </div>
+        <div>
+          <div className="BenefitImgPlaceholder"></div>
+          <h3 className="BenefitSubheading">Lorum Ipsum</h3>
+          <p className="BenefitDescription">
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+            <br />
+            Lorem ipsum dolor sit amet, consectetur
+            <br />
+            adipiscing elit, sed do eiusmod tempor.
+          </p>
+        </div>
       </div>
     </div>
   );
