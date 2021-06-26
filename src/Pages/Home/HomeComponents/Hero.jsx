ca8d60a3eccac9fd812c5eeeec6e309191450bca
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 15:12:33 +1200

    Home Hero component complete

--- src/Pages/Home/HomeComponents/Hero.jsx
+++ src/Pages/Home/HomeComponents/Hero.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./Hero.css";
-
-{
-  /*Teaching your child future proof skills */
-}
+import PlaceholderImage from "../../../Images/PlaceholderImage.png";
 
 function Hero() {
   return (
-    <div>
-      <div>
-        <h1>Hero</h1>
+    <div className="HeroContainer">
+      <div className="HeroDetails">
+        <h1 className="HeroHeader">
+          Teaching your child
+          <br />
+          future proof skills
+        </h1>
+        <p className="HeroBrief">
+          Lorem ipsum dolor sit amet, consectutue adipiscing elit, sed <br />
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className="EnrolNowBtnContainerLarge">
+          <a href="/" className="EnrolNowBtnLarge">
+            Enrol Now
+          </a>
+        </div>
       </div>
+
+      <img src={PlaceholderImage} className="HeroImg" alt="Placeholder"></img>
     </div>
   );
 }
