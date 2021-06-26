371afc7736d1bf3dbba6529f8ffd0e6f1e35ee87
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 17:58:31 +1200

    View student projects component layout complete

--- src/Pages/Home/HomeComponents/PreviewStudentProjects.jsx
+++ src/Pages/Home/HomeComponents/PreviewStudentProjects.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import "./PreviewStudentProjects.css";
 
-{
-  /*Pull from database*/
-}
-
 function PreviewStudentProjects() {
   return (
-    <div>
+    <div className="PreviewStudentProjects">
+      {/*Pull from database*/}
       <div>
-        <h1>Student Projects</h1>
+        <h1 className="PreviewProjectsHeading">View our Student Projects</h1>
+        <p className="PreviewProjectsSubheading">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et <br />
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation.
+        </p>
       </div>
+      <div className="Carousel">
+        {/*Add props -> use flex-grid*/}
+        <div>
+          <div className="PreviewProjectImg"></div>
+          <h3 className="PreviewStudentName">[Student Name]</h3>
+          <p className="PreviewAgeLevel">[9 years old, Level 8]</p>
+        </div>
+        <div>
+          <div className="PreviewProjectImg"></div>
+          <h3 className="PreviewStudentName">[Student Name]</h3>
+          <p className="PreviewAgeLevel">[9 years old, Level 8]</p>
+        </div>
+      </div>
+      <div className="PreviewNavCircles">
+        <div className="PreviewNavCircleSelected"></div>
+        <div className="PreviewNavCircle"></div>
+        <div className="PreviewNavCircle"></div>
+        <div className="PreviewNavCircle"></div>
+        <div className="PreviewNavCircle"></div>
+      </div>
+
+      <div className="PreviewGamesBtnContainer">
+        <a href="/" className="PreviewGamesBtn">
+          Play the Games Here
+        </a>
+      </div>
+      <div className="PreviewBotPadding"></div>
     </div>
   );
 }
