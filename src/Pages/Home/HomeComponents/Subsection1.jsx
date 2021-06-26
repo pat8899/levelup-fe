295f490ee29a74d212c0170547c3a94e70c4669b
Sam-McCathie <sammccathie2@gmail.com>
2021-06-26 15:32:01 +1200

    Subsection 1 component completed

--- src/Pages/Home/HomeComponents/Subsection1.jsx
+++ src/Pages/Home/HomeComponents/Subsection1.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Subsection1.css";
-
-{
-  /*Creative Problem Solving */
-}
+import PlaceholderImage from "../../../Images/PlaceholderImage.png";
 
 function Subsection1() {
   return (
-    <div>
+    <div className="Subsection1">
+      <img
+        src={PlaceholderImage}
+        alt="Placeholder"
+        className="Subsection1Img"
+      />
       <div>
-        <h1>Subsection1</h1>
+        <h2 className="Subsection1Header">Creative Problem Solving</h2>
+        <p className="Subsection1Details">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod
+          <br />
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          <br />
+          veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet,
+          consectetur
+          <br />
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et.
+        </p>
+        <div className="LearnMoreBtnContainer">
+          <a href="/" className="LearnMoreBtn">
+            Learn More
+          </a>
+        </div>
       </div>
     </div>
   );
