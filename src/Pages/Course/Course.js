5e60bd6e73721e97cde4f5a86a06929b415902bb
pat8899 <peeh.indy@gmail.com>
2021-06-27 12:29:44 +1200

    add our page - 2&3 section

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./Course.css";
 
 import CourseOne from "./CourseComponents/CourseOne";
+import CourseTwo from "./CourseComponents/CourseTwo";
+import CourseDetails3 from "./CourseComponents/CourseDetails3";
 
 function Course() {
   return (
     <div>
       <CourseOne />
+      <CourseTwo />
+      <CourseDetails3 />
+      
     </div>
   );
 }
