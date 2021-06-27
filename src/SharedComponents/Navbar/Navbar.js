ff25f7d1b1b787ce1829d3bf2509a0b26a6ed131
Sam-McCathie <sammccathie2@gmail.com>
2021-06-28 10:14:06 +1200

    improve click radius and format of buttons header

--- src/SharedComponents/Navbar/Navbar.js
+++ src/SharedComponents/Navbar/Navbar.js
@@ -5,30 +5,40 @@ function Navbar() {
   return (
     <div>
       <div className="navbar_container">
-        <a href="/" className="Links Logo">
-          LOGO
-        </a>
-        <a href="/" className="Links NavBarOption">
-          Our Course
-        </a>
-        <a href="/" className="Links NavBarOption StudentProjects">
-          Student Projects
-        </a>
-        <a href="/" className="Links NavBarOption">
-          About Us
-        </a>
-        <a href="/" className="Links NavBarOption Contact">
-          Contact
-        </a>
+        <div>
+          <a href="/" className="link">
+            <h2 className="Logo">LOGO</h2>
+          </a>
+        </div>
+        <div>
+          <a href="/" className="link">
+            <h2 className="NavBarOption">Our Course</h2>
+          </a>
+        </div>
+        <div>
+          <a href="/" className="link">
+            <h2 className="NavBarOption StudentProjects">Student Projects</h2>
+          </a>
+        </div>
+        <div>
+          <a href="/" className="link ">
+            <h2 className="NavBarOption">About Us</h2>
+          </a>
+        </div>
+        <div>
+          <a href="/" className="link ">
+            <h2 className="NavBarOption Contact">Contact</h2>
+          </a>
+        </div>
         {/*Height looks wrong might need to adjust*/}
         <div className="EnrolNowBtnContainer">
-          <a href="/" className="EnrolNowBtn">
-            Enrol Now
+          <a href="/" className="link">
+            <h2 className="EnrolNowBtn">Enrol Now</h2>
           </a>
         </div>
         <div className="StudentPortalBtnContainer">
-          <a href="/" className="StudentPortalBtn">
-            Student Portal
+          <a href="/" className="link">
+            <h2 className="StudentPortalBtn">Student Portal</h2>
           </a>
         </div>
       </div>
