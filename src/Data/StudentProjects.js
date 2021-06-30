4aa2c629d7809624470cc7b7e3820f81f0255c38
Sam-McCathie <sammccathie2@gmail.com>
2021-06-30 16:31:45 +1200

    Add more sample data

--- src/Data/StudentProjects.js
+++ src/Data/StudentProjects.js
@@ -30,4 +30,51 @@ export const StudentProjects = [
     age: 12,
     level: 87,
   },
+  {
+    // img: "",
+    studentName: "Edwards Billy",
+    age: 7,
+    level: 20,
+  },
+  {
+    studentName: "Cho v2 John",
+    age: 10,
+    level: 16,
+  },
+  {
+    studentName: "Kim Alice",
+    age: 9,
+    level: 8,
+  },
+  {
+    studentName: "Davis Stacey",
+    age: 11,
+    level: 54,
+  },
+  {
+    studentName: "Taylor Jake",
+    age: 9,
+    level: 24,
+  },
+  {
+    studentName: "Yen Cynthia",
+    age: 12,
+    level: 87,
+  },
+  {
+    // img: "",
+    studentName: "Billy Edwards 3rd",
+    age: 7,
+    level: 20,
+  },
+  {
+    studentName: "John Cho 3rd",
+    age: 10,
+    level: 16,
+  },
+  {
+    studentName: "Alice Kim 3rd",
+    age: 9,
+    level: 8,
+  },
 ];
