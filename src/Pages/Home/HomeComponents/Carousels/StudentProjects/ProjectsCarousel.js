2d3998dabbf70d612499c0b54cebfb5f5319af9c
Sam-McCathie <sammccathie2@gmail.com>
2021-07-01 16:27:17 +1200

    Replace local data with data from db & fixed bug with useEffect

--- src/Pages/Home/HomeComponents/Carousels/StudentProjects/ProjectsCarousel.js
+++ src/Pages/Home/HomeComponents/Carousels/StudentProjects/ProjectsCarousel.js
@@ -1,12 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./ProjectsCarousel.css";
-import {StudentProjects} from "../../../../../Data/StudentProjects";
+import Axios from "axios";
 import ProjectHighlight from "./ProjectHighlight";
 import ProjectPreview from "./ProjectPreview";
 import NavArrowLeft from "../../../../../Images/NavArrowLeft.png";
 import NavArrowRight from "../../../../../Images/NavArrowRight.png";
 
 function ProjectsCarousel() {
+  /*Save backend data in projectList state */
+  const [projectList, setProjectList] = useState([]);
+
+  /*Called backend whenever page is refreshed */
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setProjectList(response.data);
+    });
+  }, []);
+
   const [Container0, setContainer0] = useState(0);
   const [Container1, setContainer1] = useState(1);
   const [Container2, setContainer2] = useState(2);
@@ -14,15 +24,13 @@ function ProjectsCarousel() {
   const [Container4, setContainer4] = useState(4);
   const [Container5, setContainer5] = useState(5);
 
-  //   const length = review.length;
-
-  const Project = StudentProjects.map(function (data) {
+  const Project = projectList.map(function (data) {
     return data;
   });
+  console.log("Project");
+  console.log(Project[0]);
 
   let NumberOfProjects = Project.length - 1;
-
-  console.log(NumberOfProjects);
 
   const left = () => {
     setContainer0(Container0 === NumberOfProjects ? 0 : Container0 + 1);
@@ -50,6 +58,9 @@ function ProjectsCarousel() {
   const SP5 = Project[Container4];
   const SP6 = Project[Container5];
 
+  console.log("Projectttt");
+  console.log(Project);
+
   return (
     <div className="CarouselRow">
       <img
@@ -58,35 +69,45 @@ function ProjectsCarousel() {
         alt="Left Arrow"
         className="CarouselArrowLeft"
       />
+      {Project.length !== 0 ? (
+        <div className="CarouselProjects">
+          <ProjectPreview
+            name={SP1.studentName}
+            age={SP1.studentAge}
+            level={SP1.studentLevel}
+          />
 
-      <ProjectPreview name={SP1.studentName} age={SP1.age} level={SP1.level} />
+          <ProjectHighlight
+            name={SP2.studentName}
+            age={SP2.studentAge}
+            level={SP2.studentLevel}
+          />
 
-      <ProjectHighlight
-        name={SP2.studentName}
-        age={SP2.age}
-        level={SP2.level}
-      />
+          <ProjectHighlight
+            name={SP3.studentName}
+            age={SP3.studentAge}
+            level={SP3.studentLevel}
+          />
 
-      <ProjectHighlight
-        name={SP3.studentName}
-        age={SP3.age}
-        level={SP3.level}
-      />
+          <ProjectHighlight
+            name={SP4.studentName}
+            age={SP4.studentAge}
+            level={SP4.studentLevel}
+          />
 
-      <ProjectHighlight
-        name={SP4.studentName}
-        age={SP4.age}
-        level={SP4.level}
-      />
+          <ProjectHighlight
+            name={SP5.studentName}
+            age={SP5.studentAge}
+            level={SP5.studentLevel}
+          />
 
-      <ProjectHighlight
-        name={SP5.studentName}
-        age={SP5.age}
-        level={SP5.level}
-      />
-
-      <ProjectPreview name={SP6.studentName} age={SP6.age} level={SP6.level} />
-
+          <ProjectPreview
+            name={SP6.studentName}
+            age={SP6.studentAge}
+            level={SP6.studentLevel}
+          />
+        </div>
+      ) : null}
       <img
         src={NavArrowRight}
         onClick={right}
